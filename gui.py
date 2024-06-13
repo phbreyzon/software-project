ddcdb81740a83f3534d7ae50c7069558fc2b704d
phbreyzon <phbreyzon.4mcwd@simplelogin.co>
2024-06-13 21:42:43 +0200

    functional requirements are now fully met, the recommendation mechanism works fully

--- gui.py
+++ gui.py
@@ -12,23 +12,23 @@ class App(customtkinter.CTk):
         
         # Layout of the main app
         self.title("Music recommender")
-        self.geometry("860x520")
+        self.geometry("520x220")
         self.grid_columnconfigure(0, weight=1)
         self.scrollable_label_frame = Recommendations_list(master=self,title="recommendations", values=[""],url=[""])
                 
                 
-        self.frame_for_button_and_entry = customtkinter.CTkFrame(master=self, width=300, height=200, corner_radius=10,)
-        self.frame_for_button_and_entry.grid(row=1, column=0, sticky="nsew", padx=30, pady=(30, 10))
+        self.frame_for_button_and_entry = customtkinter.CTkFrame(master=self, width=500, height=300, corner_radius=10,)
+        self.frame_for_button_and_entry.grid(row=1, column=0, padx=20, sticky="nsew", pady=(30, 30))
                 
 
         # The entry where you enter your songs
-        self.entry = customtkinter.CTkEntry(master=self.frame_for_button_and_entry, placeholder_text="Enter recommendation", width=250)
-        self.entry.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+        self.entry = customtkinter.CTkEntry(master=self.frame_for_button_and_entry, placeholder_text="Enter recommendation", width=300)
+        self.entry.grid(row=0, column=4, padx=20, pady=20, sticky="nsew")
         
     
         
         self.button = customtkinter.CTkButton(master=self.frame_for_button_and_entry, text="recommend",command=lambda : button_action(self,self.scrollable_label_frame))
-        self.button.grid(row=1, column=1, padx=20, pady=20, sticky="nsew")
+        self.button.grid(row=1, column=4, padx=20, pady=20, sticky="nsew")
         
         
         
